5a2b753f82b82bcd6f5c4ea37edc7eceadeaa577
Daniel Mehrwald <AreaScout@users.noreply.github.com>
2015-09-30 16:38:54 +0200

    Fix: another change on pixelformat define to reflect latest naming convention

--- neo/tools/RoQPlayer/RoQPlayer.c
+++ neo/tools/RoQPlayer/RoQPlayer.c
@@ -397,7 +397,7 @@ int main(int argc, char *argv[])
 
     image = ( BYTE* )malloc( img_width * img_height * 4 * 2 );
 
-    avpicture_fill( ( AVPicture* )frame2, image, PIX_FMT_BGR32, img_width, img_height );
+    avpicture_fill( ( AVPicture* )frame2, image, AV_PIX_FMT_BGR32, img_width, img_height );
 
     img_convert_ctx = sws_getContext( dec_ctx->width, dec_ctx->height, AV_PIX_FMT_YUV444P, img_width, img_height, AV_PIX_FMT_BGR32, SWS_BICUBIC, NULL, NULL, NULL );
 
